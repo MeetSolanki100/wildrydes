1489f01f2734480907ae0616e57e2986105874d0
Meet Solanki <meetreet18@gmail.com>
2024-07-05 16:35:35 +0530

    second

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_7u7vkfCzd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7k8l0hbh9gp9r1qkm23eum2aq1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
